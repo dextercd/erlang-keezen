af8e533b619afed8ab6848e8c57a345bd547bcfe
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2021-04-03 16:03:29 +0200

    Start Cowboy web server

--- src/keezen_app.erl
+++ src/keezen_app.erl
@@ -1,8 +1,3 @@
-%%%-------------------------------------------------------------------
-%% @doc keezen public API
-%% @end
-%%%-------------------------------------------------------------------
-
 -module(keezen_app).
 
 -behaviour(application).
@@ -10,9 +5,16 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+    Dispatch = cowboy_router:compile([
+        {'_', [
+            {"/static/[...]", cowboy_static, {priv_dir, keezen, "static"}},
+            {"/", cowboy_static, {priv_file, keezen, "static/index.html"}}
+        ]}
+    ]),
+    {ok, _} = cowboy:start_clear(my_http_listener,
+                                 [{port, 8080}],
+                                 #{env => #{dispatch => Dispatch}}),
     keezen_sup:start_link().
 
 stop(_State) ->
     ok.
-
-%% internal functions
