e1baa5be7dea0ac6e58e334ebea058fc50f83e8d
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2021-04-12 14:52:51 +0200

    Return 504 when timeout

--- src/event_handler.erl
+++ src/event_handler.erl
@@ -10,14 +10,14 @@ init(Req, State) ->
     #{known := Known} = cowboy_req:match_qs([{known, int}], Req),
     case event_server:fetch_or_listen(Known) of
         {events, Events} ->
-            Req1 = reply_events(Req, Events),
+            Req1 = reply_events(200, Req, Events),
             {ok, Req1, State};
         listen ->
             start_listen(Req, State)
     end.
 
-reply_events(Req0, Events) ->
-    cowboy_req:reply(200, #{
+reply_events(Status, Req0, Events) ->
+    cowboy_req:reply(Status, #{
         <<"content-type">> => <<"application/json">>
     }, format_events(Events), Req0).
 
@@ -31,13 +31,9 @@ start_listen(Req, State) ->
     erlang:send_after(?TIMEOUT, self(), timeout),
     {cowboy_loop, Req, State}.
 
-info(timeout, Req0, State) ->
-    Req = reply_events(Req0, []),
-    {stop, Req, State};
 info({game_event, Events}, Req0, State) ->
-    Req = reply_events(Req0, Events),
+    Req = reply_events(200, Req0, Events),
     {stop, Req, State};
-info(A, Req, State) ->
-    io:format("~p", [A]),
+info(timeout, Req0, State) ->
+    Req = reply_events(504, Req0, []),
     {stop, Req, State}.
-
